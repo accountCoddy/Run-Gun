9b8136824bb2d0d475f8d8e9e05a228efae65623
accountCoddy <92232423+accountCoddy@users.noreply.github.com>
2021-10-10 10:19:54 +0300

    Leaded the game

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1633848088,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/hp.png",
+		"images/explosion-sheet0.png",
+		"images/medkid-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
